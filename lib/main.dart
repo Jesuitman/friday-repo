a5baee417951e27fd45bf6fd1d74c928729739e1
Alexandre Hastings <alex.hastings.1227@gmail.com>
2024-05-02 18:12:07 -0400

    deleted initializing code

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'screens/todo_screen.dart';
 
-import 'src/app.dart';
-import 'src/settings/settings_controller.dart';
-import 'src/settings/settings_service.dart';
 
 void main() {
   runApp(MyApp());
@@ -19,15 +16,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-  // // Set up the SettingsController, which will glue user settings to multiple
-  // // Flutter Widgets.
-  // final settingsController = SettingsController(SettingsService());
-
-  // // Load the user's preferred theme while the splash screen is displayed.
-  // // This prevents a sudden theme change when the app is first displayed.
-  // await settingsController.loadSettings();
-
-  // // Run the app and pass in the SettingsController. The app listens to the
-  // // SettingsController for changes, then passes it further down to the
-  // // SettingsView.
-  // runApp(MyApp(settingsController: settingsController));
